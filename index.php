541cf1d790852bff8194334160c4cdf60b4a56c3
Kshitij Saini <k1941996@bitbucket.org>
2020-05-29 17:17:00 +0530

    Initial Commit

--- index.php
+++ index.php
@@ -1,21 +1,17 @@
-<?php
-
-
-?>
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Hello, world!</title>
-
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    
+
+    <title>Hello, world!</title>
   </head>
   <body>
-    <h1><?php echo "<h1>hello World</h1>?></h1>
+    <h1>Hello, world!</h1>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
